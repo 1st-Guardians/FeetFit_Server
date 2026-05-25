4eba8e657ee9e589029a60fb94c0e443faf9789b
hmj6589 <hmj6589@gmail.com>
2026-05-25 19:31:39 +0900

    🧹 Chore: remove unused imports

--- src/main/java/com/feetfit/server/domain/Report.java
+++ src/main/java/com/feetfit/server/domain/Report.java
@@ -1,7 +1,6 @@
 package com.feetfit.server.domain;
 
 import com.feetfit.server.domain.common.BaseEntity;
-import com.feetfit.server.domain.enums.GaugeStatus;
 import jakarta.persistence.*;
 import lombok.*;
 
