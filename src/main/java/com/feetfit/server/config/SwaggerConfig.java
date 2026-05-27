892a5cbd539a2714e0331d4ce921fe4c3c4a67dd
7beunseo <kimes0403@gmail.com>
2026-05-28 00:31:51 +0900

    :wrench: Chore: configure CORS and Swagger server URL

--- src/main/java/com/feetfit/server/config/SwaggerConfig.java
+++ src/main/java/com/feetfit/server/config/SwaggerConfig.java
@@ -5,11 +5,19 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
+
+    @Value("${swagger.server-url:http://localhost:8080}")
+    private String serverUrl;
+
+    @Value("${swagger.server-description:Local server}")
+    private String serverDescription;
 
     @Bean
     public OpenAPI feetFitSwaggerAPI() {
@@ -22,6 +30,9 @@ public class SwaggerConfig {
                         .scheme("bearer")
                         .bearerFormat("JWT"));
         return new OpenAPI()
+                .addServersItem(new Server()
+                        .url(serverUrl)
+                        .description(serverDescription))
                 .addSecurityItem(securityRequirement)
                 .components(components)
                 .info(new Info()
