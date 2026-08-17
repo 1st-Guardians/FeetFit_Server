3a0ba8abbba30a9905a01245304f1e7b6b160f25
7beunseo <kimes0403@gmail.com>
2026-08-17 18:06:26 +0900

    🐛 Fix: correct plantar footprint id column name

--- src/main/java/com/feetfit/server/domain/PlantarFootprint.java
+++ src/main/java/com/feetfit/server/domain/PlantarFootprint.java
@@ -28,7 +28,7 @@ public class PlantarFootprint extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "tina_pedis_analysis_id")
+    @Column(name = "planter_footprint_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
